2e6bdfc16d9a606457e7e2759b3ce3f91ea43cd9
GustaffWell <gustaff_well95@mail.ru>
2023-06-04 14:21:11 +0500

    Добавлен функционал добавления тегов к задаче(без фронта)

---
+++ src/main/java/com/javarush/jira/bugtracking/TaskController.java
@@ -0,0 +1,28 @@
+package com.javarush.jira.bugtracking;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = TaskController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
+@Slf4j
+public class TaskController {
+
+    static final String REST_URL = "/api/tasks";
+    private TaskService taskService;
+
+    @GetMapping("/{taskId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Transactional
+    public String addTag(@PathVariable Long taskId, @RequestParam String tag) {
+        log.debug("add tag={} for taskId={}", tag, taskId);
+        taskService.addTag(taskId, tag);
+
+        return "index";
+    }
+}
